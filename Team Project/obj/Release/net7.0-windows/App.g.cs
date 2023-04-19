0e8fc69c07be1ca0c3ecf56fd79ddf5b47defafe
NyiteX <109142191+NyiteX@users.noreply.github.com>
2023-04-19 14:59:14 +0300

    adadaw

--- Team Project/obj/Release/net7.0-windows/App.g.cs
+++ Team Project/obj/Release/net7.0-windows/App.g.cs
@@ -59,7 +59,7 @@ namespace Team_Project {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Team Project;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Team Project;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Team Project/obj/Release/net7.0-windows/MainWindow.g.cs
+++ Team Project/obj/Release/net7.0-windows/MainWindow.g.cs
@@ -31,6 +31,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Shell;
 using Team_Project;
+using WpfAnimatedGif;
 
 
 namespace Team_Project {
@@ -229,7 +230,7 @@ namespace Team_Project {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Team Project;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Team Project;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
